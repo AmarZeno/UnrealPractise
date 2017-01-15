5ecbbbadce036205874b710518c2be611b082fc5
Amar Zeno <Amar Zeno>
2017-01-14 17:07:22 -0700

    Section 16 done

--- Section_02/Section_02/main.cpp
+++ Section_02/Section_02/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	// introduce the game
 	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Welcome to Bulls and Cows\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	
+	cout << endl;
 	return 0;
 }
